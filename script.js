e0901be5f1cbe490ccadae3c534f53d691762222
Loris Cuntreri <62082748+MarelGuy@users.noreply.github.com>
2020-10-08 16:59:03 -0300

    es di oggi

---
+++ script.js
@@ -0,0 +1,37 @@
+// Sets important constants and variables
+
+
+let rows = document.getElementsByClassName("gridRow");
+let cells = document.getElementsByClassName("cell");
+
+// Creates a default grid sized 16x16
+function defaultGrid() {
+    makeRows(76);
+    makeColumns(76);
+}
+
+// Takes (rows, columns) input and makes a grid
+const container = document.getElementById("container");
+
+function makeRows(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (c = 0; c < (rows * cols); c++) {
+        let cell = document.createElement("div");
+        cell.innerText = (c + 1);
+        container.appendChild(cell).className = "grid-item";
+    };
+};
+
+makeRows(16, 16);
+
+// Creates columns
+function makeColumns(cellNum) {
+    for (i = 0; i < rows.length; i++) {
+        for (j = 0; j < cellNum; j++) {
+            let newCell = document.createElement("div");
+            rows[j].appendChild(newCell).className = "cell";
+        };
+
+    };
+};
